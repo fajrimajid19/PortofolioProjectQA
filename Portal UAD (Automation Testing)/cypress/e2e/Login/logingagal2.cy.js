335450bd57a26c83fb9a1d017071501a4d4501fc
fajrimajid19 <48217170+fajrimajid19@users.noreply.github.com>
2023-10-11 05:27:29 +0700

    Delete Portal UAD (Automation Testing)/cypress directory

--- Portal UAD (Automation Testing)/cypress/e2e/Login/logingagal2.cy.js
+++
@@ -1,7 +0,0 @@
-it('Testing Portal UAD', function() {
-    cy.visit('https://portal.uad.ac.id/login')
-    cy.get(':nth-child(1) > .input-icon > .form-control').type(' ')
-    cy.get(':nth-child(2) > .input-icon > .form-control').type('180018300')
-    cy.get('.form-actions > .btn').click()
-
-})
--- Portal UAD (Automation Testing)/cypress/e2e/Login/logingagal4.cy.js
+++
@@ -1,7 +0,0 @@
-it('Testing Portal UAD', function() {
-    cy.visit('https://portal.uad.ac.id/login')
-    cy.get(':nth-child(1) > .input-icon > .form-control').type('1800018300')
-    cy.get(':nth-child(2) > .input-icon > .form-control').type('12345678')
-    cy.get('.form-actions > .btn').click()
-
-})
